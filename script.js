f806cacc63722de50133dc92250487d002926e51
ilaydamul <ilaydamulazimoglu@gmail.com>
2024-04-18 19:01:55 +0300

    ilan detay ve profil sayfaları tamamlandı

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+
+
+
+
+$("#advert").click(function () {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+        title: "Başvuru Yap",
+        text: "İlana başvuru yapmak istediğinize emin misiniz?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Evet",
+        cancelButtonText: "Hayır",
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire({
+                title: "Başarılı",
+                text: "İlan başvurunuz başarıyla yapıldı.",
+                icon: "success",
+                confirmButtonText: "Kapat",
+            });
+        }
+    });
+})
+
+$("#update").click(function () {
+    Swal.fire({
+        icon: "success",
+        title: "Başarılı!",
+        showConfirmButton: false,
+        timer: 1500
+    });
+})
