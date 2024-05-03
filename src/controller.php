e5f62dae6ecc4a844f81f17667700c6db7027a70
Barisdemirci3 <baris19052003@gmail.com>
2024-05-03 19:17:54 +0300

    PostgreSQL connection added, First data pushed.

--- src/controller.php
+++ src/controller.php
@@ -1,6 +1,7 @@
 <?php 
-$sonuc = [];
+require_once("db.php");
 if(isset($_POST["register_name"])){
+    $sonuc = array();
     $name = $_POST["register_name"];
     $surname = $_POST["surname"];
     $username = $_POST["username"];
@@ -8,7 +9,9 @@ if(isset($_POST["register_name"])){
     $address = $_POST["address"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    
+    pg_insert($conn, 'kullanicilar', array('kullanici_ad' => $name, 'kullanici_soyad' => $surname, 'kullanici_nickname' => $username, 'kullanici_telefon_no' => $phone_no, 'kullanici_adres' => $address, 'kullanici_email' => $email, 'kullanici_sifre' => $password));
+    pg_close($conn);
+
 }
 echo json_encode($sonuc);
 ?>
--- src/db.php
+++ src/db.php
@@ -4,10 +4,4 @@ $dbname = 'postgres';
 $username = 'barisdemirci';
 // PostgreSQL veritabanına bağlan
 $conn = pg_connect("host=$host dbname=$dbname user=$username");
-if (!$conn) {
-    echo "Veritabanına bağlanırken hata oluştu.";
-} else {
-    echo "Veritabanına başarıyla bağlandı.";
-}
-pg_close($conn);
 ?>
