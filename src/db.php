d17111c0ea1f54975b879cff618fcfe762028cb7
Barisdemirci3 <baris19052003@gmail.com>
2024-05-12 21:27:34 +0300

    Stop tracking src/db.php

--- src/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$dbname = 'postgres';
-$username = 'barisdemirci';
-// PostgreSQL veritabanına bağlan
-$conn = pg_connect("host=$host dbname=$dbname user=$username"); 
-echo "Deneme";
-echo "Basdasd";
-?>
