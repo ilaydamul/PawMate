a92764d9ed10790699853e6171e69155d00bbc22
Barisdemirci3 <baris19052003@gmail.com>
2024-05-12 21:32:35 +0300

    Personal db connection file added.

--- src/your-db.php
+++ src/your-db.php
@@ -1,7 +1,9 @@
 <?php
 $host = 'localhost';
-$dbname = 'postgres';
-$username = 'barisdemirci';
+$dbname = 'veritabanı_adı';
+$username = 'kullanıcı_adı';
+$port = '5432';
+$password = 'şifre';
 // PostgreSQL veritabanına bağlan
-$conn = pg_connect("host=$host dbname=$dbname user=$username"); 
+$conn = pg_connect("host=$host dbname=$dbname user=$username port=$port password=$password"); 
 ?>
