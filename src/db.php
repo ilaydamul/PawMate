157a364025318e0d49e87a43b41cb53184b23278
Barisdemirci3 <baris19052003@gmail.com>
2024-05-15 23:34:35 +0300

    db added.

---
+++ src/db.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$dbname = 'proje_teslimi_deneme_sunucusu';
+$username = 'barisdemirci';
+$password = "";
+// Eğer passwordunuz varsa alttaki connect kısmına password=$password şeklinde ekleyebilirsiniz.
+// PostgreSQL veritabanına bağlan
+$conn = pg_connect("host=$host dbname=$dbname user=$username port=5432"); 
+?>
