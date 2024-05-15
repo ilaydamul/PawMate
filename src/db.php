679416db248e10d74601fc647f409eb39ca36c81
Barisdemirci3 <baris19052003@gmail.com>
2024-05-16 00:03:22 +0300

    files updated.

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'pawmate';
-$username = 'postgres';
-$password = "1q2w3e4rA.";
+$dbname = 'proje_teslimi_deneme_sunucusu';
+$username = 'barisdemirci';
+$password = "";
 // Eğer passwordunuz varsa alttaki connect kısmına password=$password şeklinde ekleyebilirsiniz.
 // PostgreSQL veritabanına bağlan
 $conn = pg_connect("host=$host dbname=$dbname user=$username port=5432 password=$password"); 
